46022db2f0592789772dfe99f0774508c1e28955
unknown <B01672A@W5061050.ena.us.experian.local>
2013-03-27 12:00:15 -0400

    commit code

---
+++ JaspersoftRESTClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JaspersoftRESTClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Organization org = new Organization("organisation_142");
+            Console.WriteLine(org.Exists());
+            org.Create("organizarion_100");
+        }
+    }
+}
---
+++ JaspersoftRESTClient/Services/Domain.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Xml;
+
+namespace JaspersoftRESTClient
+{
+    class Domain
+    {
+        private readonly string _organizationId;
+        private string _tanent;
+
+        public Domain(string organizationId)
+        {
+            _organizationId = organizationId;
+        }
+
+        public bool Exists()
+        {
+            var cookieContainer = new CookieContainer();
+            var request = WebRequest.Create("http://localhost:8080/jasperserver-pro/rest/login?j_username=superuser&j_password=superuser") as HttpWebRequest;
+            request.CookieContainer = cookieContainer;
+            request.Method = "GET";
+            var response = request.GetResponse() as HttpWebResponse;
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                request = WebRequest.Create("http://localhost:8080/jasperserver-pro/rest/resources/organizations/organisation_142/Domains?q=domain_142") as HttpWebRequest;
+                request.CookieContainer = cookieContainer;
+                request.Method = "GET";
+                response = request.GetResponse() as HttpWebResponse;
+                var stream = response.GetResponseStream();
+                var reader = new StreamReader(stream, Encoding.UTF8);
+                Char[] read = new Char[256];
+                var count = reader.Read(read, 0, 256);
+                var builder = new StringBuilder();
+                builder.Append(new String(read, 0, count));
+                while (count > 0)
+                {
+                    count = reader.Read(read, 0, 256);
+                    builder.Append(new String(read, 0, count));
+                }
+                reader.Close();
+                _tanent = builder.ToString();
+                response.Close();
+            }
+
+            return _tanent != null;
+        }
+
+        public void Create(string organizationId)
+        {
+            if (_tanent != null)
+            {
+                var cookieContainer = new CookieContainer();
+                var request = WebRequest.Create("http://localhost:8080/jasperserver-pro/rest/login?j_username=superuser&j_password=superuser") as HttpWebRequest;
+                request.CookieContainer = cookieContainer;
+                request.Method = "GET";
+                var response = request.GetResponse() as HttpWebResponse;
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var dataToSend = _tanent.Replace(_organizationId, organizationId);
+                    request = WebRequest.Create("http://localhost:8080/jasperserver-pro/rest/resources/organizations/") as HttpWebRequest;
+                    request.CookieContainer = cookieContainer;
+                    request.Method = "PUT";
+                    request.ContentType = "text/plain";
+                    request.ContentLength = dataToSend.Length;
+                    using (var writer = new StreamWriter(request.GetRequestStream()))
+                    {
+                        writer.Write(dataToSend);
+                    }
+
+                    response = request.GetResponse() as HttpWebResponse;
+                    if (response.StatusCode != HttpStatusCode.Created)
+                    {
+                        throw new ApplicationException("Could not create Organisation");
+                    }
+                }
+            }
+        }
+    }
+}
